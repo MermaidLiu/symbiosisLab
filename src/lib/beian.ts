237a2b1bb6a37590c2bd727fca351905397e59f1
Mermaid-Liu <simplelive2013@126.com>
2026-07-31 16:09:56 +0800

    Update ICP beian number to 京ICP备2025140042号-2.

--- src/lib/beian.ts
+++ src/lib/beian.ts
@@ -1,3 +1,3 @@
 /** ICP 备案号（页脚展示，链接至工信部备案官网） */
-export const ICP_BEIAN_NUMBER = "京0166ce3bdf0eff661232c65189cd6b43";
+export const ICP_BEIAN_NUMBER = "京ICP备2025140042号-2";
 export const ICP_BEIAN_URL = "https://beian.miit.gov.cn/";
